785fb8aee3f6437dafc0b727a36009bf03304fef
chetana777 <62290457+chetana777@users.noreply.github.com>
2020-06-24 15:23:56 +0530

    Python Task 2

--- Python-Task2/lists.py
+++ Python-Task2/lists.py
@@ -5,45 +5,45 @@ numbers = [25,69,78,35,24,69,12]
 print("The list of numbers : ",numbers)
 
 
-# 1)using append() method:
+# 1)using append() method to apend new element at the end of list:
 numbers.append(85)
-print("\nThe list of numbers after using append method: ",numbers)
+print("The list of numbers after using append method: ",numbers)
 
 
-# 2)using insert() method:
+# 2)using insert() method to insert new element into list:
 numbers.insert(2,45)
-print("\nThe list of numbers after using insert method: ",numbers)
+print("The list of numbers after using insert method: ",numbers)
 
 
-# 3)using remove() method:
+# 3)using remove() method to remove element:
 numbers.remove(69)
-print("\nThe list of numbers after using remove method: ",numbers)
+print("The list of numbers after using remove method: ",numbers)
 
 
-# 4)using pop() method:
+# 4)using pop() method to delete last element:
 
 #passing index in pop() method:
 numbers.pop(4)
-print("\nThe list of numbers after using pop method with index: ",numbers)
+print("The list of numbers after using pop method with index: ",numbers)
 
 #without passing index in pop() method:
 numbers.pop()
-print("\nThe list of numbers after using pop method without index: ",numbers)
+print("The list of numbers after using pop method without index: ",numbers)
 
 
-# 5)using extend() method:
+# 5)using extend() method to combine elements to the list:
 numbers.extend([5,8,6])
-print("\nThe list of numbers after using extend method: ",numbers)
+print("The list of numbers after using extend method: ",numbers)
 
 
-# 6)using min() method:
+# 6)using min() method to get the minimum element:
 numbers1=min(numbers)
-print("\nThe minimum number from the list numbers is: ",numbers1)
+print("The minimum number from the list numbers is: ",numbers1)
 
 
-# 7)using sum() method:
+# 7)using sum() method to get sum of all elements:
 numbers2=sum(numbers)
-print("\nThe sum of all elements in the list numbers is: ",numbers2)
+print("The sum of all elements in the list numbers is: ",numbers2)
 
 
 
