0ac864864580ccf1d5c2082502b7c9a64c2de113
chetana777 <62290457+chetana777@users.noreply.github.com>
2020-06-24 15:30:00 +0530

    Python task 2

--- Python-Task2/tuples.py
+++ Python-Task2/tuples.py
@@ -6,42 +6,42 @@ numbers = (25,69,78,35,24,69,12,78)
 print("The tuple of numbers : ",numbers)
 
 
-# 1)using count() method:
+# 1)using count() method to count how many times the element is present in the tuple:
 count = numbers.count(78)
-print("\nThe occurence of 78 in the numbers tuple is: ",count)
+print("The occurence of 78 in the numbers tuple is: ",count)
 
 
-# 2)using index() method:
+# 2)using index() method to find index:
 index = numbers.index(69)
-print("\nThe index of 69 is: ",index)
+print("The index of 69 is: ",index)
 
 
-# 3)using length() method:
+# 3)using length() method to get length:
 length = len(numbers)
-print("\nThe length of the numbers tuple is: ",length)
+print("The length of the numbers tuple is: ",length)
 
 
-# 4)using max() method:
+# 4)using max() method to get maximum element:
 maximum_number = max(numbers)
-print("\nThe maximum number of the numbers tuple is: ",maximum_number)
+print("The maximum number of the numbers tuple is: ",maximum_number)
 
 
 
-# 5)using min() method:
+# 5)using min() method to get minimum element:
 minimum_number = min(numbers)
-print("\nThe minimum number of the numbers tuple is: ",minimum_number)
+print("The minimum number of the numbers tuple is: ",minimum_number)
 
 
 
-# 6)using sum() method:
+# 6)using sum() method to calculate sum:
 summation =sum (numbers)
-print("\nThe sum of all elements in the tuple numbers is: ",summation)
+print("The sum of all elements in the tuple numbers is: ",summation)
 
 
 # 7)conversion of list to tuple:
 list = [1,2,3,4]
 Tuple = tuple(list)
-print("\nThe tuple obtained by converting a list into tuple is: ",Tuple)
+print("The tuple obtained by converting a list into tuple is: ",Tuple)
 
 
 
